27585510511ecb28616b6b013a75dee7d1ead3a5
LukeZurg22 <11780262+LukeZurg22@users.noreply.github.com>
2024-10-30 15:10:52 -0500

    Git Changes

--- AlgorithmicUnitTesting/obj/Debug/net6.0/AlgorithmicUnitTesting.AssemblyInfo.cs
+++ AlgorithmicUnitTesting/obj/Debug/net6.0/AlgorithmicUnitTesting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmicUnitTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b26a883518ed4e1e8f1fa327324adf8172d2a017")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmicUnitTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmicUnitTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
